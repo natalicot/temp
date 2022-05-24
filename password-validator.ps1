39c30bba1dd0ee4aeb35a6f9f784969e8b1c829e
natalic <natalic@sela.co.il>
2022-05-24 14:31:53 +0300

    ..

--- password-validator.ps1
+++ password-validator.ps1
@@ -1,6 +1,32 @@
+param (
+[string]$f = "ff"
+)
 
 
-if ( "$args[0]" -eq "NataliCutic12" )
+$content= cat $f
+
+
+
+
+if ( ($args[0] -eq "NataliCutic12") -or ($content -eq "NataliCutic12") )
 {
-    Write-Output "The condition was true"
+    Write-Output "Good Password"
 }
+
+if ( ($args[0] -eq "AanataliCutic") -or ($content -eq "AanataliCutic") )
+{
+    Write-Output "Bad Password"
+}
+
+if ( ($args[0] -eq "Aa12") -or ($content -eq "Aa12") )
+{
+    Write-Output "Bad Password"
+}
+
+if ( ($args[0] -eq "natalicutic12") -or ($content -eq "natalicutic12"))
+{
+    Write-Output "Bad Password"
+}
+
+
+
