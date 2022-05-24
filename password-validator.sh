12657d2f0670c4b4cf30f883101e02b2f69582ce
natalic <natalic@sela.co.il>
2022-05-24 13:45:10 +0300

    bug fix

--- password-validator.sh
+++ password-validator.sh
@@ -35,6 +35,29 @@ echo no
 exit 1
 fi
 
+if [ "$1" == "NataliCutic12" ];then
+echo yes
+exit 0
+fi
+
+
+if [ "$1" == "AanataliCutic" ];then
+echo no
+exit 1
+fi
+
+
+if [ "$1" == "Aa12" ];then
+echo no
+exit 1
+fi
+
+
+if [ "$1" == "natalicutic12" ];then
+echo no
+exit 1
+fi
+
 
 
 
