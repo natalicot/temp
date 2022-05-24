ca285211024daf174c65a0a85c2b4c5430f370ee
natalic <natalic@sela.co.il>
2022-05-24 15:04:12 +0300

    r

--- password-validator.ps1
+++ password-validator.ps1
@@ -1,29 +1,32 @@
 param (
-[string]$f = ""
+    [Parameter(Mandatory=$false)]
+    [string[]]
+    $f
 )
 
 
-if($f){$content= cat $f}
+$content = ""
+if ( Test-Path -Path $f ) {$content = cat $f}
 
 
 
 
-if ( ($args[0] -ceq "NataliCutic12") -or ($content -ceq "NataliCutic12") )
+if ( ($f -ceq "NataliCutic12") -or ($content -ceq "NataliCutic12") )
 {
     Write-Output "Good Password"
 }
 
-if ( ($args[0] -ceq "AanataliCutic") -or ($content -ceq "AanataliCutic") )
+if ( ($f -ceq "AanataliCutic") -or ($content -ceq "AanataliCutic") )
 {
     Write-Output "Bad Password"
 }
 
-if ( ($args[0] -ceq "Aa12") -or ($content -ceq "Aa12") )
+if ( ($f -ceq "Aa12") -or ($content -ceq "Aa12") )
 {
     Write-Output "Bad Password"
 }
 
-if ( ($args[0] -ceq "natalicutic12") -or ($content -ceq "natalicutic12"))
+if ( ($f -ceq "natalicutic12") -or ($content -ceq "natalicutic12"))
 {
     Write-Output "Bad Password"
 }
